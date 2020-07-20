7b42168bb8a237aa4a6189568cdb01b495ec940b
Amjed Saleel K <amjedsaleel@gmail.com>
2020-07-20 22:01:56 +0530

    modified

--- test1/settings.py
+++ test1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p23gm+j%1zv=h9@t$r*g+m+w45z^pv#ie2=4koc+=jj5zuc%=q'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = ['heroku-host-testing.herokuapp.com', '127.0.0.1']
 
